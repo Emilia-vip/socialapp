752030f5cc82e2a49e3fb7977753397832081454
ahmadardal <ahmadardal@live.se>
2026-04-29 11:36:14 +0200

    Temporarily bake in the api url

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -5,4 +5,5 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export const API_URL = import.meta.env.BUN_PUBLIC_API_URL;
+// export const API_URL = import.meta.env.BUN_PUBLIC_API_URL;
+export const API_URL = "https://socialapp-u7hp.onrender.com";
